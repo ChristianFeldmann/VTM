22f914878734068f9b601cbfc796f182f36b98cc
Xiang Li <xlxiangli@tencent.com>
2018-10-23 14:52:30 -0700

    Integration of JVET-L0553 on the fix of initial QP signaling

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -50,6 +50,8 @@
 #include <assert.h>
 #include <cassert>
 
+#define JVET_L0553_FIX_INITQP                             1
+
 #define JVET_L0147_ALF_SUBSAMPLED_LAPLACIAN               1 // Subsampled Laplacian calculation
 
 #define JVET_L0191_LM_WO_LMS                              1 // NO LMS regression. min/max are used instead

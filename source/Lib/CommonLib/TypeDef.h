b54b6014bc339a9c73556fc20bc98eb4cf825c08
Xiang Li <xlxiangli@tencent.com>
2018-11-02 14:55:58 -0700

    JVET-L0410: On deblocking tC table

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -50,6 +50,8 @@
 #include <assert.h>
 #include <cassert>
 
+#define JVET_L0410_TC_TAB                                 1 // Change TC table for QP 51-63
+
 #define JVET_L0136_L0085_LM_RESTRICTED_LINEBUFFER         1 // line buffer restriction in LM prediction
 
 #define JVET_L0059_MTS_SIMP                               1 // Simpification on MTS signaling

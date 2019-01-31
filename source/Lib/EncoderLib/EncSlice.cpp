3d0f0acb726757dc3a3dec99c6c44a4788f1d0d0
Li Jingya <jingya.li@sg.panasonic.com>
2019-01-31 11:31:50 +0800

    M0823: Encoder optimization for MMVD

--- source/Lib/EncoderLib/EncSlice.cpp
+++ source/Lib/EncoderLib/EncSlice.cpp
@@ -1427,10 +1427,6 @@ void EncSlice::checkDisFracMmvd( Picture* pcPic, uint32_t startCtuTsAddr, uint32
   {
     pcSlice->setDisFracMMVD( true );
   }
-#if JVET_M0312_FRACMMVD_SWITCH_FOR_UHD
-  bool useIntegerMVD = (pcPic->lwidth()*pcPic->lheight() > 1920 * 1080);
-  pcSlice->setDisFracMMVD(useIntegerMVD);
-#endif
 }
 #endif
 

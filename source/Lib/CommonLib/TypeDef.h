e04ad0cf27a29bb2f84c8324e38f74d261ef6c80
jamesxxiu <xiaoyuxiu@kwai.com>
2019-04-02 02:08:48 -0700

    JVET-N0335: MV Rounding Unification

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -50,6 +50,8 @@
 #include <assert.h>
 #include <cassert>
 
+#define JVET_N0335_N0085_MV_ROUNDING                      1  // MV rounding unification
+
 #define JVET_N0477_LMCS_CLEANUP                           1
 #define JVET_N0220_LMCS_SIMPLIFICATION                    1
 

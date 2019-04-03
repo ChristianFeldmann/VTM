e7c76c37e8c229a63f0bf712441fbee9efb9d456
Xiang Li <xlxiangli@tencent.com>
2019-04-03 17:35:10 +0200

    Change tabs to spaces

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -73,7 +73,7 @@
 
 #define JVET_N0271_SIMPLFIED_CCLM                         1 // Simplified CCLM parameter derivation in JVET-N0271
 
-#define JVET_N0178_IMPLICIT_BDOF_SPLIT					  1
+#define JVET_N0178_IMPLICIT_BDOF_SPLIT                    1
 
 #define JCTVC_Y0038_PARAMS                                1
 

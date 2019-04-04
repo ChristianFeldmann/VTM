c6c0e648b9a5ee6480cb7654a7352501e73f34fc
Xiaozhong Xu <xiaozhongxu@tencent.com>
2019-04-03 22:32:51 -0400

    re-arrange IBC search range for small CTU sizes

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -50,6 +50,8 @@
 #include <assert.h>
 #include <cassert>
 
+#define JVET_N0175_N0251_N0384_IBC_SMALL_CTU              1 // IBC search range arrangement for small CTU sizes
+
 #define JVET_N0286_SIMPLIFIED_GBI_IDX                     1 // Simplified coding of the GBi index
 
 #define JVET_N600_AMVR_TPM_CTX_REDUCTION                  1

4b1fe19d573951702f4716f88dd910e9eefa196e
Kammachi Sreedhar <kashyap.kammachi-sreedhar@nokia.com>
2019-04-12 11:13:53 +0300

    NAL reader tracing function updated_v2

--- source/Lib/DecoderLib/NALread.cpp
+++ source/Lib/DecoderLib/NALread.cpp
@@ -111,6 +111,10 @@ static void xTraceNalUnitHeader(InputNALUnit& nalu)
 {
   DTRACE( g_trace_ctx, D_NALUNITHEADER, "*********** NAL UNIT (%s) ***********\n", nalUnitTypeToString(nalu.m_nalUnitType) );
   #if JVET_N0067_NAL_Unit_Header
+  bool m_zeroTidRequiredFlag = 0;
+  if((nalu.m_nalUnitType >= 16) && (nalu.m_nalUnitType <= 31)) {
+    m_zeroTidRequiredFlag = 1;
+  }
   DTRACE( g_trace_ctx, D_NALUNITHEADER, "%-50s u(%d)  : %u\n", "zero_tid_required_flag", 1, m_zeroTidRequiredFlag );
   DTRACE( g_trace_ctx, D_NALUNITHEADER, "%-50s u(%d)  : %u\n", "nuh_temporal_id_plus1", 3, nalu.m_temporalId + 1 );
   DTRACE( g_trace_ctx, D_NALUNITHEADER, "%-50s u(%d)  : %u\n", "nal_unit_type_lsb", 4, (nalu.m_nalUnitType) - (m_zeroTidRequiredFlag << 4));

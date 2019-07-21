877e2af590da2cd7bff7ef9c4ec54a1db9c1f7ba
Xin Zhao <xinzzhao@tencent.com>
2019-07-20 22:44:30 -0700

    Additional comment regarding to MAX_NUM_TUS.

--- source/Lib/CommonLib/CommonDef.h
+++ source/Lib/CommonLib/CommonDef.h
@@ -315,7 +315,7 @@ static const int MIN_CU_LOG2 =                                      2;
 static const int MIN_PU_SIZE =                                      4;
 static const int MAX_NUM_PARTS_IN_CTU =                         ( ( MAX_CU_SIZE * MAX_CU_SIZE ) >> ( MIN_CU_LOG2 << 1 ) );
 #if JVET_O0545_MAX_TB_SIGNALLING
-static const int MAX_NUM_TUS =                                     16;
+static const int MAX_NUM_TUS =                                     16; ///< Maximum number of TUs within one CU. When max TB size is 32x32, up to 16 TUs within one CU (128x128) is supported
 static const int MAX_LOG2_DIFF_CU_TR_SIZE =                         3;
 #else
 static const int MAX_LOG2_DIFF_CU_TR_SIZE =                         2;

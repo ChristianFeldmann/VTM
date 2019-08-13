cdc75715c97b93e58c2497bbe56aa7bfdb85e952
Nan Hu <nanh@qti.qualcomm.com>
2019-08-13 15:34:25 -0700

    add alf_coeff_abs_prefix trace at decoder

--- source/Lib/DecoderLib/VLCReader.cpp
+++ source/Lib/DecoderLib/VLCReader.cpp
@@ -2797,7 +2797,7 @@ int HLSyntaxReader::alfGolombDecode( const int k, const bool signed_val )
 #if RExt__DECODER_DEBUG_BIT_STATISTICS
     xReadFlag( b, "" );
 #else
-    xReadFlag( b );
+    READ_FLAG( b, "alf_coeff_abs_prefix");
 #endif
   }
 

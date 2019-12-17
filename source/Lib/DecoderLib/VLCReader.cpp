4bcb2e1aa39ec296c4e474eede2cd35e6e04c0ef
Takeshi Tsukuba <takeshi.tsukuba@sony.com>
2019-12-17 09:31:08 +0900

    Rename log2_max_transform_skip_block_size_minus2 in SW to log2_transform_skip_max_size_minus2 in WD.

--- source/Lib/DecoderLib/VLCReader.cpp
+++ source/Lib/DecoderLib/VLCReader.cpp
@@ -594,7 +594,7 @@ void HLSyntaxReader::parsePPS( PPS* pcPPS, ParameterSetManager *parameterSetMana
   }
 
   READ_SVLC(iCode, "init_qp_minus26" );                            pcPPS->setPicInitQPMinus26(iCode);
-  READ_UVLC(uiCode, "log2_max_transform_skip_block_size_minus2");
+  READ_UVLC(uiCode, "log2_transform_skip_max_size_minus2");
   pcPPS->setLog2MaxTransformSkipBlockSize(uiCode + 2);
 
   READ_FLAG( uiCode, "cu_qp_delta_enabled_flag" );            pcPPS->setUseDQP( uiCode ? true : false );

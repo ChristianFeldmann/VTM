827cc860c1772a96604a725cd0605b2bb146479c
Karsten Suehring <karsten.suehring@hhi.fraunhofer.de>
2020-01-15 17:17:22 +0100

    update copyright header to include 2020

--- source/Lib/CommonLib/SampleAdaptiveOffset.cpp
+++ source/Lib/CommonLib/SampleAdaptiveOffset.cpp
@@ -3,7 +3,7 @@
  * and contributor rights, including patent rights, and no such rights are
  * granted under this license.
  *
- * Copyright (c) 2010-2019, ITU/ISO/IEC
+ * Copyright (c) 2010-2020, ITU/ISO/IEC
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without

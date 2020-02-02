793b112693ae1bec6b7c6e6b4a9349e2d8421626
jiechen <jiechen.cj@alibaba-inc.com>
2020-02-02 18:18:15 +0800

    update codes of JVET-Q0416

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -50,7 +50,7 @@
 #include <assert.h>
 #include <cassert>
 
-#define JVET_Q0416_WRAPAROUND_OFFSET                      1  //JVET-Q0416: subtract ï¼ˆCtbSizeY / MinCbSizeY + 2ï¼‰ from wraparound offset before signaling
+#define JVET_Q0416_WRAPAROUND_OFFSET                      1  //JVET-Q0416: subtract £¨CtbSizeY / MinCbSizeY + 2£© from wraparound offset before signaling
 
 #define JVET_Q0505_CHROAM_QM_SIGNALING_400                1  //JVET-Q0505: Cleanup of chroma quantization matrix signaling for 400 color format
 
--- source/Lib/DecoderLib/VLCReader.cpp
+++ source/Lib/DecoderLib/VLCReader.cpp
@@ -1772,7 +1772,7 @@ void HLSyntaxReader::parseSPS(SPS* pcSPS)
   if (pcSPS->getWrapAroundEnabledFlag())
   {
 #if JVET_Q0416_WRAPAROUND_OFFSET
-    READ_UVLC(uiCode, "sps_ref_wraparound_offset_minus1");               pcSPS->setWrapAroundOffset((uiCode + 2 + (1 << pcSPS->getLog2DiffMaxMinCodingBlockSize()))*(1 << pcSPS->getLog2MinCodingBlockSize()));
+    READ_UVLC(uiCode, "sps_ref_wraparound_offset");               pcSPS->setWrapAroundOffset((uiCode + 2 + (1 << pcSPS->getLog2DiffMaxMinCodingBlockSize()))*(1 << pcSPS->getLog2MinCodingBlockSize()));
 #else
     READ_UVLC(uiCode, "sps_ref_wraparound_offset_minus1");               pcSPS->setWrapAroundOffset( (uiCode+1)*(1 <<  pcSPS->getLog2MinCodingBlockSize()));
 #endif

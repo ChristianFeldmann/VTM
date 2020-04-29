f4502c0a306e23359e71544b304abf87648c7907
Muhammed Coban <mcoban@qti.qualcomm.com>
2020-04-28 22:27:49 -0700

    JVET-R0083: SW cleanup addressing ticket #859 regarding DQ + TS(RRC) combination issue from Q0089.

--- source/Lib/CommonLib/DepQuant.cpp
+++ source/Lib/CommonLib/DepQuant.cpp
@@ -1574,9 +1574,9 @@ DepQuant::~DepQuant()
 void DepQuant::quant( TransformUnit &tu, const ComponentID &compID, const CCoeffBuf &pSrc, TCoeff &uiAbsSum, const QpParam &cQP, const Ctx& ctx )
 {
 #if JVET_R0271_SLICE_LEVEL_DQ_SDH_RRC
-  if( tu.cs->slice->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || (tu.mtsIdx[compID] != MTS_SKIP)) )
+  if( tu.cs->slice->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || tu.mtsIdx[compID] != MTS_SKIP) )
 #else
-  if( tu.cs->picHeader->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || (tu.mtsIdx[compID] != MTS_SKIP)) )
+  if( tu.cs->picHeader->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || tu.mtsIdx[compID] != MTS_SKIP) )
 #endif
   {
     //===== scaling matrix ====
@@ -1604,9 +1604,9 @@ void DepQuant::quant( TransformUnit &tu, const ComponentID &compID, const CCoeff
 void DepQuant::dequant( const TransformUnit &tu, CoeffBuf &dstCoeff, const ComponentID &compID, const QpParam &cQP )
 {
 #if JVET_R0271_SLICE_LEVEL_DQ_SDH_RRC
-  if( tu.cs->slice->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || (tu.mtsIdx[compID] != MTS_SKIP)) )
+  if( tu.cs->slice->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || tu.mtsIdx[compID] != MTS_SKIP) )
 #else
-  if( tu.cs->picHeader->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || (tu.mtsIdx[compID] != MTS_SKIP)) )
+  if( tu.cs->picHeader->getDepQuantEnabledFlag() && (tu.cu->slice->getTSResidualCodingDisabledFlag() || tu.mtsIdx[compID] != MTS_SKIP) )
 #endif
   {
     const int         qpDQ            = cQP.Qp(tu.mtsIdx[compID] == MTS_SKIP) + 1;

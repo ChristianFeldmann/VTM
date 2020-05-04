3dc26db40be8d28667848b6938b1895edd2420a3
LI JINGYA <jingya.li@sg.panasonic.com>
2020-05-04 17:03:43 +0200

    Update EncLib.cpp

--- source/Lib/EncoderLib/EncLib.cpp
+++ source/Lib/EncoderLib/EncLib.cpp
@@ -1220,7 +1220,7 @@ void EncLib::xInitSPS( SPS& sps )
     sps.setBitDepth      (ChannelType(channelType), m_bitDepth[channelType] );
     sps.setQpBDOffset  (ChannelType(channelType), (6 * (m_bitDepth[channelType] - 8)));
 #if JVET_R0045_TS_MIN_QP_CLEANUP
-    sps.setInteralMinusInputBitDepth(ChannelType(channelType), max(0, (m_bitDepth[channelType] - m_inputBitDepth[channelType])));
+    sps.setInternalMinusInputBitDepth(ChannelType(channelType), max(0, (m_bitDepth[channelType] - m_inputBitDepth[channelType])));
 #else
     sps.setMinQpPrimeTsMinus4(ChannelType(channelType), max(0, 6 * (m_bitDepth[channelType] - m_inputBitDepth[channelType])));
 #endif

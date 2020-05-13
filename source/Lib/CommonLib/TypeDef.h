41bfec38dd09a6d1cad611e137046cfc49d82bf7
phil <cowanp@sharplabs.com>
2020-05-13 13:55:50 -0700

    JVET-R0112: Picture header semantics cleanup for gdr_or_irap_flag

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -171,6 +171,8 @@
 
 #define JVET_R0108_DCI_SIGNALING                          1 // JVET-R0108 Proposal 1 DCI signaling changes
 
+#define JVET_R0112_SEMANTICS_CLEANUP                      1 // JVET-R0112: Picture header semantics cleanup for gdr_or_irap_flag
+
 //########### place macros to be be kept below this line ###############
 
 #define JVET_R0164_MEAN_SCALED_SATD                       1 // JVET-R0164: Use a mean scaled version of SATD in encoder decisions

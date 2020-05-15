86a5fc1a2d69ae3abc89e715da0d977fbe718261
phil <cowanp@sharplabs.com>
2020-05-15 14:01:11 -0700

    JVET-R0099: Proposal 2 and 3, DPB and HRD parameter signaling for OLS

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -183,6 +183,8 @@
 
 #define JVET_R0107_VPS_SIGNALING                          1 // JVET-R017: Proposal 2 VPS signaling change and updated inference rule
 
+#define JVET_R0099_DPB_HRD_PARAMETERS_SIGNALLING          1   // JVET-R0099: DPB and HRD parameter signalling for OLS
+
 //########### place macros to be be kept below this line ###############
 
 #define JVET_R0164_MEAN_SCALED_SATD                       1 // JVET-R0164: Use a mean scaled version of SATD in encoder decisions

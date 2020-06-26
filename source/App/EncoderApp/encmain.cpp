1d3b234811eeb3d51693693fef7b093e5c525549
Karsten Suehring <karsten.suehring@hhi.fraunhofer.de>
2020-06-26 15:58:57 +0200

    Fix encoder command line parsing for layer-wise parameters
    At the encoder there are long parameters like "--QP=32" and short parameters like "-q 32"
    
    For layer parameters -lx (with x being the layer index) the encoder always assumes that
    -lx is followed by two parameters, which is only true for short type parameters.
    
    This checks for "--" in the first parameter following -lx and only reads a second parameter,
    if "--" is not found at the beginning.

--- source/App/EncoderApp/encmain.cpp
+++ source/App/EncoderApp/encmain.cpp
@@ -140,13 +140,30 @@ int main(int argc, char* argv[])
       {
         if( argv[i][0] == '-' && argv[i][1] == 'l' )
         {
+          int incr = 0; // count how many parameters are consumed
           if( argv[i][2] == std::to_string( layerIdx ).c_str()[0] )
           {
+            if (argc <= i + 1)
+            {
+              THROW("Command line parsing error: missing parameter after -lx\n");
+            }
             layerArgv[j] = argv[i + 1];
-            layerArgv[j + 1] = argv[i + 2];
-            j += 2;
+            incr++;
+            // check for long parameters, which start with "--"
+            const std::string param = argv[i + 1];
+            if (param.rfind("--", 0) != 0)
+            {
+              // only short parameters have a second parameter for the value
+              if (argc <= i + 2)
+              {
+                THROW("Command line parsing error: missing parameter after -lx\n");
+              }
+              layerArgv[j + 1] = argv[i + 2];
+              incr++;
+            }
+            j+= incr;
           }
-          i += 2;
+          i += incr;
         }
         else
         {
